5233352881e7dd09a26eaa9e4ee549e3e5ad4b34
malyinik <Djaikmorh2005@gmail.com>
2026-02-20 01:49:28 +0300

    feat: добавить lualine, bufferline, dashboard

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,99 @@
+return {
+  {
+    -- Нижняя строка состояния
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    config = function()
+      require('lualine').setup({})
+    end,
+  },
+
+  {
+    -- Отображения открытых буферов в виде вкладок
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+
+    opts = {
+      vim.keymap.set('n', '<leader>blp', ':BufferLinePick<CR>', { desc = '[B]uffer[l]ine [P]ick' }),
+      vim.keymap.set('n', '<leader>blc', ':BufferLinePickClose<CR>', { desc = '[B]uffer[l]ine [C]lose' }),
+    },
+
+    config = function()
+      local bufferline = require('bufferline')
+      bufferline.setup({
+        options = {
+          mode = 'tabs', -- set to "tabs" to only show tabpages instead
+          diagnostics = 'nvim_lsp',
+          separator_style = 'thick',
+        },
+      })
+    end,
+  },
+
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup({
+        shortcut_type = 'letter', -- shortcut type 'letter' or 'number'
+        config = {
+          header = {}, -- header
+          week_header = {
+            enable = true, --boolean use a week header
+          },
+          disable_move = true, -- boolean default is false disable move key
+
+          shortcut = {
+            {
+              desc = '󰊳 Update',
+              group = '@property',
+              action = 'Lazy update',
+              key = 'u',
+            },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+
+          packages = { enable = true }, -- show how many plugins neovim loaded
+          project = {
+            enable = true,
+            limit = 8,
+            icon = '󰪺 ',
+            label = 'Recent projects',
+            action = 'Telescope find_files cwd=',
+          },
+          mru = {
+            enable = true,
+            limit = 16,
+            icon = ' ',
+            label = 'Recent files',
+            cwd_only = false,
+          },
+
+          footer = {}, -- footer
+        },
+      })
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+}

b33049908cab1b6caf50b48f7d806cea38ead804
malyinik <Djaikmorh2005@gmail.com>
2026-02-20 00:53:38 +0300

    feat: добавить конфигурацию Neovide

---
+++ lua/core/neovide.lua
@@ -0,0 +1,23 @@
+if vim.g.neovide then
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_cursor_animation_length = 0.25
+
+  vim.o.guifont = 'JetBrainsMonoNL Nerd Font Mono:h14'
+
+  vim.keymap.set('n', '<A-CR>', function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end)
+
+  vim.keymap.set('n', '<C-=>', function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1
+  end)
+
+  vim.keymap.set('n', '<C-->', function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.1
+  end)
+
+  vim.keymap.set('n', '<C-0>', function()
+    vim.g.neovide_scale_factor = 1.0
+  end)
+end

8e7e664014b4c22882b0db42660947cdb964251f
malyinik <Djaikmorh2005@gmail.com>
2026-02-22 19:29:34 +0300

    feat: добавить lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,20 @@
+-- Вызов lazygit внутри Neovim
+return {
+  'kdheepak/lazygit.nvim',
+  lazy = true,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+
+  keys = {
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  },
+}

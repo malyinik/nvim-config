1f01924640dffc10822991fa0772e2a3f67def4b
malyinik <Djaikmorh2005@gmail.com>
2026-02-22 19:23:55 +0300

    feat: добавить which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+-- Подсказки клавиш при нажатии leader и других префиксов
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+
+  opts = {
+    preset = 'modern',
+    spec = {
+      { '<leader>s', group = 'Search' },
+      { '<leader>t', group = 'Toggle' },
+      { '<leader>bl', group = 'Bufferline' },
+      { 'gr', group = 'LSP' },
+      { ']', group = 'Next' },
+      { '[', group = 'Prev' },
+    },
+  },
+}

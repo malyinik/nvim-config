8327e57f276b07e3da9ef4df5c3551431dbf4548
malyinik <Djaikmorh2005@gmail.com>
2026-02-22 21:06:57 +0300

    feat: добавить noice.nvim + nvim-notify

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    -- Улучшенные уведомления
+    'rcarriga/nvim-notify',
+    opts = {
+      timeout = 3000,
+      render = 'wrapped-compact',
+    },
+  },
+
+  {
+    -- Улучшенная командная строка, уведомления и сообщения
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+    },
+  },
+}
